bb9e9345a5b2a5919f56ec866269a0517a3d91cd
Matt Favoino <mattfavoino@gmail.com>
2018-03-12 15:11:32 -0700

    Minor styling, adding structure to html for charts, chart.js example rendering.

--- app/static/app.js
+++ app/static/app.js
@@ -10,7 +10,9 @@ $(document).ready(function() {
             dataType: 'json',
             data: {'username': JSON.stringify(username)},
             success: function(response) {
+                console.log(response)
                 localStorage.setItem('userinfo', JSON.stringify(response))
+                $('.welcome').hide()
                 renderBasicInfo()
                 getAllBeers(username)
             },
@@ -28,8 +30,7 @@ function getAllBeers(username) {
         dataType: 'json',
         data: {'username': JSON.stringify(username)},
         success: function(response) {
-            console.log('The beers are here')
-            console.log(response)
+          console.log(response)
         },
         error: function(error) {
             console.log(error)
@@ -81,5 +82,46 @@ function renderBasicInfo() {
             </tbody>
           </table>`
 
+    let welcome = `<a class="nav-link" href="#">Welcome, ${username}!</a>`
+    $('.nav-link').hide()
+    $('.nav-item').html(welcome)
     $('#user-card').html(table)
 };
+
+// let ctx = $("#time-graph");
+// let timeChart = new Chart(ctx, {
+//     type: 'pie',
+//     data: {
+//         labels: [],
+//         datasets: [{
+//             label: 'Beer Styles',
+//             data: [12, 19, 3, 5, 2, 3],
+//             backgroundColor: [
+//                 'rgba(255, 99, 132, 0.2)',
+//                 'rgba(54, 162, 235, 0.2)',
+//                 'rgba(255, 206, 86, 0.2)',
+//                 'rgba(75, 192, 192, 0.2)',
+//                 'rgba(153, 102, 255, 0.2)',
+//                 'rgba(255, 159, 64, 0.2)'
+//             ],
+//             borderColor: [
+//                 'rgba(255, 99, 132, 1)',
+//                 'rgba(54, 162, 235, 1)',
+//                 'rgba(255, 206, 86, 1)',
+//                 'rgba(75, 192, 192, 1)',
+//                 'rgba(153, 102, 255, 1)',
+//                 'rgba(255, 159, 64, 1)'
+//             ],
+//             borderWidth: 1
+//         }]
+//     },
+//     options: {
+//         scales: {
+//             yAxes: [{
+//                 ticks: {
+//                     beginAtZero:true
+//                 }
+//             }]
+//         }
+//     }
+// });
